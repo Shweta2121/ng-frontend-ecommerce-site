0eb0eadd86c94fb01a797dfac2aecee9a04694a5
Shweta Singh <shweta.singh@knoxpo.com>
2021-03-09 10:55:16 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-test-site-frontend';
+}
